a314470ae99db8bf748c708b680f75b846814bd0
nurbe18072012-dotcom <nurbe18072012@gmail.com>
2026-02-10 15:38:47 +0500

    jizax

--- script.js
+++ script.js
@@ -1,53 +1,12 @@
-var a = "Enter name "
-//console.log(a.charAt(a.length -1 ));
-//console.log(a.charAt(a.length -1 ));
+var entername = prompt("Enter your name").trim()
 
-//console.log(a.charCodeAt(3));
-//console.log(a.charCodeAt(7));
-
-//console.log(a.codePointAt(0));
-//console.log(a.codePointAt(9));
-
-/*console.log(a.at(-2));
-console.log(a.at(2));*/
-
-/*console.log(a[-1]);
-console.log(a[-2]);*/
-
-/*console.log(a.slice(-1));
-console.log(a.substring(2));*/
-
-/*console.log(a.isWellFormed());
-console.log(a.notWellFormed(-7));*/
-
-//console.log(a.padStart(10, "*"));
-
-/*console.log(a.repeat(100));
-console.log(a.repeat(20));*/
-
-/*/console.log(a.replace(/word/g, "gadoy"));
-console.log(a.replace(/word/, "gadoy"));*/
-
-
-
-
-
-
-
-
-
-
-
-
-var ism = prompt("Enter your name").trim()
-
-console.log("salom" + " " + ${ism} + " " + "yaxshimisiz");
-if(ism==""){
+console.log("salom" + " " + `${entername}` + " " + "yaxshimisiz");
+if(entername==""){
     alert("enter your name")
-    var ism = prompt("You didnt enter your name");
+    var entername = prompt("You didnt enter your name");
 }else if(ism.length <=3){
     alert("Name is very short")
-     var ism = prompt("Enter your name").trim()
+     var entername = prompt("Enter your name").trim()
 }
  var Why = prompt("why do you want to enter the institute")
  if(Why.length <=10) {
