49db30046cc2f721d8cbbc877237b831473a6bda
nurbe18072012-dotcom <nurbe18072012@gmail.com>
2026-02-17 15:59:39 +0500

    first nurbek

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-var entername = prompt("Enter your name").trim()
 
-console.log("salom" + " " + `${entername}` + " " + "yaxshimisiz");
-if(entername==""){
-    alert("enter your name")
-    var entername = prompt("You didnt enter your name");
-}else if(ism.length <=3){
-    alert("Name is very short")
-     var entername = prompt("Enter your name").trim()
+
+let text = prompt("Matn kiriting:");
+let count = {};
+
+for (let i = 0; i < text.length; i++) {
+    let harf = text[i];
+
+    if (count[harf]) {
+        count[harf]++;
+    } else {
+        count[harf] = 1;
+    }
 }
- var Why = prompt("why do you want to enter the institute")
- if(Why.length <=10) {
-    alert("You entered too little information, please enter more than 10 characters.")
-    console.log("You didn't write why you wanted to enter the institute.");
-    
- }else{
-    alert("Thanks")
- }
- console.log(Why.replaceAll("universitet", "Alicode"));
- 
+
+for (let key in count) {
+    console.log(key + " = " + count[key]);
+}
